443d8be04c510b92c647218637fe2da950293eb9
Alexandre REGNERE <a.r.@airdealexandre.home>
2020-06-22 15:55:34 +0200

    Rajout d'un calcul + entête

--- test.c
+++ test.c
@@ -1,15 +1,17 @@
+// Vérification de fonction
 #include<stdio.h>
 
 float fonc(float x) {
         float s = x*x ;
-        return s;
+        return s ;
 }
 
 int main(void) {
 
 
     float s = fonc(3) ;
-    printf("Fonc = %.3f\n", s) ;
+    printf("fonc(3) = %.3f\n", s) ;
+    printf("f(4) = %.3f\n", fonc(4)) ;
 
 return 0 ;
 }
