b47fdc772b6ceade3516f5497b9a6c1379b840ae
Alexandre REGNERE <a.r.@airdealexandre.home>
2020-06-22 18:12:44 +0200

    modif pour la branch1

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 // Vérification de fonction
+// Ceci est modifié dans la branche fork-test
 #include<stdio.h>
 
 float fonc(float x) {
@@ -12,6 +13,7 @@ int main(void) {
     float s = fonc(3) ;
     printf("fonc(3) = %.3f\n", s) ;
     printf("f(4) = %.3f\n", fonc(4)) ;
+    printf("f(5) = %.3f\n", fonc(5)) ; //ajouté avec la branche
 
 return 0 ;
 }
